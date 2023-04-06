aded309da3cbdd0dc27f94ce17e73297fa6ddf02
sks <ssurovneva@mail.ru>
2023-04-06 19:25:18 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            //Form2.Show(); // вызов немодального окна (не блокирует рабочий процесс)
+            Form2.ShowDialog(); // вызов модального окна (перехватит на себя всё управление (пока не закончим с ней работу нельзя будет перейти на первую))
+            
+            MessageBox.Show("End of programm");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+
+        // 1
+        /* static DialogResult ShowMesageBox()
+        {
+           string message = "Window show text";
+            string caption = "Window with two buttons";
+            DialogResult res = MessageBox.Show(message, caption, MessageBoxButtons.OKCancel);
+            string button = res.ToString();
+
+            res = MessageBox.Show($"You presseed button {button}. Retry? ", button, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Asterisk);
+
+            return res;
+        }*/
+
+        // 2
+       /* static DialogResult ShowMesageBox()
+        {
+            DialogResult res = MessageBox.Show("Error. No disk access", "Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            if(res == DialogResult.Abort)
+            {
+                MessageBox.Show("You pressed button abort");
+            }
+            else if (res == DialogResult.Retry)
+            {
+                MessageBox.Show("You pressed button retry");
+            }
+            else if (res == DialogResult.Ignore)
+            {
+                MessageBox.Show("You pressed button ignore");
+            }
+
+            return res;
+        }*/
+
+
+        [STAThread]
+
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            // 1
+
+            /*DialogResult result;
+            do
+            {
+                result = ShowMesageBox();
+            } while (result == DialogResult.Retry);*/
+
+            // 2
+
+            /* DialogResult result;
+             do
+             {
+                 result = ShowMesageBox();
+             } while (result != DialogResult.Abort);*/
+
+            // Модальное и немодальное окно
+        }
+    }
+
+}
